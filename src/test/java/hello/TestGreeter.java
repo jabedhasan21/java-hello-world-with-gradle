4f769ededce808b82c41986ab082c2b2b91e3c8b
Ted Lehr <t_l81@txstate.edu>
2018-10-24 15:48:59 -0500

    Replace junit4 imports with junit5 imports

--- src/test/java/hello/TestGreeter.java
+++ src/test/java/hello/TestGreeter.java
@@ -1,29 +1,15 @@
 package hello;
 
-
-
-import static org.junit.Assert.*;
-
-
-
-import org.junit.Before;
-
-import org.junit.Test;
-
-
-
-
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class TestGreeter {
 
-
-
    private Greeter g;
 
-	
-
    @Before
-
    public void setUp() throws Exception 
 
    {
@@ -32,32 +18,23 @@ public class TestGreeter {
 
    }
 
-
-
    @Test
-
    public void testGreeterEmpty() 
 
    {
 
       assertEquals(g.getName(),"");
-
       assertEquals(g.sayHello(),"Hello!");
 
    }
 
-	
-
    @Test
-
    public void testGreeter() 
 
    {
 
       g.setName("World");
-
       assertEquals(g.getName(),"World");
-
       assertEquals(g.sayHello(),"Hello World!");
 
    }
