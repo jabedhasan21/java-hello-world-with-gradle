c96197019adc6ec338f40525d34007a081be3496
Ted Lehr <t_l81@txstate.edu>
2018-10-30 16:27:34 -0500

    Just making the code pretty

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,7 +5,8 @@ package hello;
 import org.joda.time.LocalTime;
 
 public class HelloWorld {
-  public static void main(String[] args) {
+  public static void main(String[] args) 
+  {
     LocalTime currentTime = new LocalTime();
     System.out.println("Who will say the local time is: " + currentTime);
     
