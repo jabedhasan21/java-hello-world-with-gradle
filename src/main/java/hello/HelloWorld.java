778166fc7acdd86c65e99d6586e752fe42dad881
jk1457 <47014351+jk1457@users.noreply.github.com>
2019-03-07 17:18:11 -0600

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
   public static void main(String[] args) {
     LocalTime currentTime = new LocalTime();
     System.out.println("The current local time is Beers Thirty: " + currentTime);
-    
+    System.out.println("Beers Thirty is now over. Switch to Whiskey O'Clock: " + currentTime);
     Greeter greeter = new Greeter();
     System.out.println(greeter.sayHello());
   }
